f492fed9ea483af12a341ce03550137f24f972ef
Matt <42049737+MattBelworthy@users.noreply.github.com>
2019-08-29 16:58:27 +1200

    Add files via upload

--- SSI.py
+++ SSI.py
@@ -17,13 +17,13 @@ class SSITest(unittest.TestCase):
         self.browser.get('https://forms-uat.kiwirail.co.nz/Designer/Runtime/Form/Site+Safety+Inspection')
         #find the username field and enter the email example@yahoo.com.
         username = self.browser.find_elements_by_css_selector("input[name=loginfmt]")
-        username[0].send_keys('USERNAME')
+        username[0].send_keys('matt.belworthy.lewthwaite@kiwirail.co.nz')
         #find the next button and click it.
         nextButton = self.browser.find_elements_by_xpath("//input[@value='Next']")[0]
         nextButton.click()
         #find the password field and enter the password password.
         password = self.browser.find_elements_by_css_selector("input[name=passwd]")
-        password[0].send_keys('PASSWORD')
+        password[0].send_keys('Funfun121')
         #explicitly wait for object to be present.
         self.wait.until(ec.visibility_of_element_located((By.XPATH, "//input[@value='Sign in']")))
         #find the login button and click it.
@@ -43,15 +43,22 @@ class SSITest(unittest.TestCase):
         "Submits a form with no filled in fields and expects an error"
         #find the submit button and click it.
         submitButton = self.browser.find_elements_by_xpath("//a[@name='btnSubmit']")
+        print("SUUUUUUUUUUUUUUUUUUUUUUUUUUUB")
+        print(submitButton)
         submitButton[0].click()
         #explicitly wait for object to be present.
-        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//a[text()='OK']")))
+        testing = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//a[text()='OK']")))
         #find the ok button and click it.
         okButton = self.browser.find_elements_by_xpath("//a[text()='OK']")
         okButton[0].click()
 
-    def test_empty_form(self):
-        "Submits a form with no filled in fields and expects an error"
+    def test_no_area_conditions(self):
+        "Submit form with no area conditions. Expecting missing mandatory fields error"
+        #find the region drop down box and change selected field.
+        dropDown = self.browser.find_elements_by_xpath("//div[@class='input-control select-box dropdown-box watermark' and @id='b54e7f6c-1707-842e-eae9-89faba43736b_9a91a61f-34da-273c-9bae-0c078787702e']")
+        dropDown[0].click()
+        selectPlatform = self.browser.find_elements_by_xpath("//ul/li[@title='Auckland']")
+        selectPlatform[0].click()
         #find the submit button and click it.
         submitButton = self.browser.find_elements_by_xpath("//a[@name='btnSubmit']")
         submitButton[0].click()
